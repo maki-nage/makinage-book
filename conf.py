89ff5380590c9d631ec091c55f48132ead60a350
Romain Picard <romain.picard@oakbits.com>
2020-08-29 17:32:38 +0200

    remove source code

--- conf.py
+++ conf.py
@@ -78,6 +78,8 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_show_sourcelink = False
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
